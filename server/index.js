61c44c8d8a047fc4041e9401e37e96373a239ab9
glinfico <104463743+glinfico@users.noreply.github.com>
2026-09-18 16:25:54 -0400

    Implement custom CORS middleware
    Replace cors() middleware with custom CORS headers.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,13 @@ import Stripe from 'stripe';
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 
 app.use(express.json());
     'http://localhost:3000',
